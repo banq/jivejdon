023614e712010922e6c63dad9aca56b6168273dc
banq <banq@163.com>
2018-08-20 10:02:37 +0800

    doc

--- readme.cmd
+++ readme.cmd
@@ -1,5 +1,7 @@
-Jivejdon forum/blog/CMS platform
+Jivejdon
+=========================================
 
 Jivejdon is a example(DDD + CQRS) developed by jdonframework, it has running  for over ten years
-at: https://www.jdon.com/forum
+at: [https://www.jdon.com/forum](https://www.jdon.com/forum)
+
 
