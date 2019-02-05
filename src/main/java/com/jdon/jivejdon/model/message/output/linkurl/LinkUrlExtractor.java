a9ee0f013ac437941596206ed45673e1553fbbb8
banq <banq@163.com>
2019-02-05 21:33:13 +0800

    fix bug, seperate messageUrlVO from messageVO

---
+++ src/main/java/com/jdon/jivejdon/model/message/output/linkurl/LinkUrlExtractor.java
@@ -0,0 +1,4 @@
+package com.jdon.jivejdon.model.message.output.linkurl;
+
+public class LinkUrlExtractor {
+}
---
+++ src/main/java/com/jdon/jivejdon/model/message/output/thumbnailUrl/ThumbnailExtractorBeanInfo.java
@@ -0,0 +1,4 @@
+package com.jdon.jivejdon.model.message.output.thumbnailUrl;
+
+public class ThumbnailExtractorBeanInfo {
+}
