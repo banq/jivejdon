e3ffa630765606adff410ee134e35f0f1c27c9d6
banq <banq@163.com>
2019-06-15 08:59:41 +0800

    fix qiniu bug

--- src/main/java/com/jdon/jivejdon/event/domain/consumer/write/postThread/CDNRefreshSubsciber.java
+++ src/main/java/com/jdon/jivejdon/event/domain/consumer/write/postThread/CDNRefreshSubsciber.java
@@ -1,14 +1,9 @@
 package com.jdon.jivejdon.event.domain.consumer.write.postThread;
 
 import com.jdon.annotation.Component;
-import com.jdon.jivejdon.util.ToolsUtil;
 import com.qiniu.cdn.CdnManager;
 import com.qiniu.cdn.CdnResult;
 import com.qiniu.common.QiniuException;
-import com.qiniu.common.Zone;
-import com.qiniu.http.Response;
-import com.qiniu.storage.BucketManager;
-import com.qiniu.storage.Configuration;
 import com.qiniu.util.Auth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,23 +28,23 @@ public class CDNRefreshSubsciber {
 		String secretKey = "sGSeH06-V2jiW3gKqzq7R0Rg0ZpJ576G_laLL2AK";
 		Auth auth = Auth.create(accessKey, secretKey);
 
-		Zone z = Zone.zone0();
-		Configuration c = new Configuration(z);
+//		Zone z = Zone.zone0();
+//		Configuration c = new Configuration(z);
 
-		//实例化一个BucketManager对象
-		BucketManager bucketManager = new BucketManager(auth, c);
-		//要测试的空间和key，并且这个key在你空间中存在
-		String bucket = "cdnjdon";
-		String key = fileurl;
-		try {
-			//调用delete方法移动文件
-			bucketManager.delete(bucket, key);
-		} catch (QiniuException e) {
-			//捕获异常信息
-			Response r = e.response;
-			System.out.println(r.toString());
-			logger.error("cdn delete error=" + r.toString());
-		}
+//		//实例化一个BucketManager对象
+//		BucketManager bucketManager = new BucketManager(auth, c);
+//		//要测试的空间和key，并且这个key在你空间中存在
+//		String bucket = "cdnjdon";
+//		String key = fileurl;
+//		try {
+//			//调用delete方法移动文件
+//			bucketManager.delete(bucket, key);
+//		} catch (QiniuException e) {
+//			//捕获异常信息
+//			Response r = e.response;
+//			System.out.println("cdn delete error=" + fileurl + " error:" + r.toString());
+//			logger.error("cdn delete error=" + fileurl + " error:" + r.toString());
+//		}
 
 		CdnManager cdn = new CdnManager(auth);
 		//待刷新的链接列表
@@ -60,10 +55,10 @@ public class CDNRefreshSubsciber {
 		try {
 			//单次方法调用刷新的链接不可以超过100个
 			CdnResult.RefreshResult result = cdn.refreshUrls(urls);
-			logger.debug(" cdn refresh" + result.code);
+			logger.debug(urls + " cdn refresh" + result.code);
 			//获取其他的回复内容
 		} catch (QiniuException e) {
-			System.err.println(urls + e.response.toString());
+			System.err.println("cdn refres url=" + urls + " error:" + e.response.toString());
 			logger.error("cdn refres url=" + urls + " error:" + e.response.toString());
 		}
 	}
@@ -78,7 +73,7 @@ public class CDNRefreshSubsciber {
 
 		public void run() {
 			try {
-				if (ToolsUtil.isDebug()) return;
+				//if (ToolsUtil.isDebug()) return;
 				refreshCDN(fileurl);
 			} catch (Exception e) {
 				e.printStackTrace();
