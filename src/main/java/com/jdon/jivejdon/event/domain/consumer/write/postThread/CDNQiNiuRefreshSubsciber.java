78c91ca0a0aabad0ad8008104db5ad23decc4e1f
banq <banq@163.com>
2019-06-24 14:38:35 +0800

    fix bug

--- src/main/java/com/jdon/jivejdon/event/domain/consumer/write/postThread/CDNQiNiuRefreshSubsciber.java
+++ src/main/java/com/jdon/jivejdon/event/domain/consumer/write/postThread/CDNQiNiuRefreshSubsciber.java
@@ -4,6 +4,10 @@ import com.jdon.annotation.Component;
 import com.qiniu.cdn.CdnManager;
 import com.qiniu.cdn.CdnResult;
 import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.BucketManager;
+import com.qiniu.storage.Configuration;
 import com.qiniu.util.Auth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,7 +18,7 @@ public class CDNQiNiuRefreshSubsciber {
 
 	public static void main(String[] args) {
 		CDNQiNiuRefreshSubsciber cDNQiNiuRefreshSubsciber = new CDNQiNiuRefreshSubsciber();
-		cDNQiNiuRefreshSubsciber.cdnRefresh("rss");
+		cDNQiNiuRefreshSubsciber.cdnRefresh("query/approved");
 		System.out.println("qq");
 	}
 
@@ -34,6 +38,24 @@ public class CDNQiNiuRefreshSubsciber {
 		String accessKey = "axCnOZ5hHeMMJLejjKhh7O56JdxAGpmEgY11G3EB";
 		String secretKey = "sGSeH06-V2jiW3gKqzq7R0Rg0ZpJ576G_laLL2AK";
 		Auth auth = Auth.create(accessKey, secretKey);
+
+		Zone z = Zone.zone0();
+		Configuration c = new Configuration(z);
+
+		//实例化一个BucketManager对象
+		BucketManager bucketManager = new BucketManager(auth, c);
+		//要测试的空间和key，并且这个key在你空间中存在
+		String bucket = "cdnjdon";
+		String key = fileurl;
+		try {
+			//调用delete方法移动文件
+			bucketManager.delete(bucket, key);
+		} catch (QiniuException e) {
+			//捕获异常信息
+			Response r = e.response;
+			System.out.println(key + " " + r.toString());
+			logger.error("cdn delete error=" + r.toString() + " " + key);
+		}
 
 		CdnManager cdn = new CdnManager(auth);
 		//待刷新的链接列表
