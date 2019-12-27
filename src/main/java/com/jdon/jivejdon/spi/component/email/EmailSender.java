46e3bc3a5d898bcf373450c7fde4caa533f33f63
banq <banq@163.com>
2019-12-27 15:48:13 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/spi/component/email/EmailSender.java
+++ src/main/java/com/jdon/jivejdon/spi/component/email/EmailSender.java
@@ -49,7 +49,7 @@ public class EmailSender implements Startable {
 	public void send(EmailVO emailVO) {
 		try {
 			Message message = createMessage(emailVO);
-			System.err.println("sending email2 : " + message.getAllRecipients());
+			System.err.println("sending email2 : " + message.getSubject());
 			if (message != null)
 				Transport.send(message);
 		} catch (Exception e) {
