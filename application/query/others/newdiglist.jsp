738d44aa517273860ceeae1f75bd1fc158425cf5
banq <banq@163.com>
2021-05-26 20:49:25 +0800

    fixed

--- application/query/others/newdiglist.jsp
+++ application/query/others/newdiglist.jsp
@@ -14,8 +14,49 @@ com.jdon.jivejdon.util.ToolsUtil.setHeaderCache(10 * 60, request, response);
 String coutlength = (String)pageContext.getAttribute("count");
 %>        
 <logic:iterate indexId="i"   id="forumThread" name="threadListForm" property="list" length='<%=coutlength%>' >
-	<div class="info">
-         <a href="<%=request.getContextPath()%>/<bean:write name="forumThread" property="threadId"/>" target="_blank" class="smallgray"><bean:write name="forumThread" property="name" /></a>
+  <bean:define id="forumMessage" name="forumThread" property="rootMessage" />
+  <div class="box"> 
+  <div class="linkblock">
+  <div class="row">
+       <div class="col-sm-3">
+           <div class="wrap-vid">
+              <div class="thumbn">
+                <logic:notEmpty name="forumMessage" property="messageUrlVO.thumbnailUrl">
+                  <img src="<bean:write name="forumMessage" property="messageUrlVO.thumbnailUrl"/>" border='0' class="thumbnail" loading="lazy"/>
+                </logic:notEmpty>
+              </div>         
+           </div>
+       </div> 
+       <div class="col-sm-9">
+         <div class="box">
+             <bean:define id="body" name="forumMessage" property="messageVO.body" />
+
+             <h3 class="vid-name"><a href="<%=request.getContextPath()%>/<bean:write name="forumThread" property="threadId"/>"><bean:write name="forumThread" property="name"/></a></h3>
+      
+            <div class="info">			 
+              <span><i class="fa fa-calendar"></i>
+                <bean:define id="cdate" name="forumThread" property="creationDate" ></bean:define>
+               <%String cdateS = (String)pageContext.getAttribute("cdate"); %><%=cdateS.substring(2, 11) %>
+                      </span>
+			 <logic:notEqual name="forumThread" property="state.messageCount" value="0">
+              <span><i class="fa fa-comment"></i> <bean:write name="forumThread" property="state.messageCount" />
+                      </span>
+		       </logic:notEqual>  
+              <span><i class="fa fa-eye"></i><bean:write name="forumThread" property="viewCount" />
+                      </span>
+			   <logic:notEqual name="forumMessage" property="digCount" value="0">
+                       <span><i class="fa fa-heart"></i>
+                      <bean:write name="forumMessage" property="digCount"/>
+					   </span>
+                      </logic:notEqual>     
+			 <span><i class="fa fa-user"></i><bean:write name="forumThread" property="rootMessage.account.username" />
+               </span>
+                      
+             </div>
+         </div>
       </div>
+  </div>
+  </div>
+  </div>      
 </logic:iterate>
 </div>
--- src/main/java/com/jdon/jivejdon/presentation/action/ViewThreadAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/ViewThreadAction.java
@@ -1,73 +1,35 @@
 package com.jdon.jivejdon.presentation.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.jdon.controller.WebAppUtil;
-import com.jdon.jivejdon.spi.component.throttle.hitkey.CustomizedThrottle;
-import com.jdon.jivejdon.spi.component.throttle.hitkey.HitKeyIF;
-import com.jdon.jivejdon.spi.component.throttle.hitkey.HitKeySame;
-import com.jdon.jivejdon.spi.component.viewcount.ThreadViewCounterJob;
-import com.jdon.jivejdon.domain.model.ForumThread;
-import com.jdon.jivejdon.presentation.filter.SpamFilterTooFreq;
 import com.jdon.jivejdon.api.query.ForumMessageQueryService;
+import com.jdon.jivejdon.domain.model.ForumThread;
+import com.jdon.jivejdon.presentation.form.ThreadForm;
+import com.jdon.strutsutil.FormBeanUtil;
+import com.jdon.strutsutil.ModelDispAction;
 import com.jdon.util.Debug;
-import com.jdon.util.UtilValidate;
-import org.apache.struts.action.Action;
+
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.regex.Pattern;
+public class ViewThreadAction extends ModelDispAction {
 
-public class ViewThreadAction extends Action {
-
-	private CustomizedThrottle customizedThrottle;
 	private ForumMessageQueryService forumMessageQueryService;
-	private ThreadViewCounterJob threadViewCounterJob;
 
-	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm,
-								 HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		response.getWriter().close();
-//		String requestType = request.getHeader("X-Requested-With");
-//		if (requestType == null || !requestType.equalsIgnoreCase("XMLHttpRequest")) {
-//			response.sendError(404);
-//			return null;
-//		}
-		if (!request.getProtocol().contains("2"))//http\2
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String threadId = request.getParameter("threadId");
+		if (threadId == null || threadId.length() == 0)
 			return null;
-
-		String threadId = request.getParameter("thread");
-		if ((threadId == null) || (!UtilValidate.isInteger(threadId)) || threadId.length() > 10) {
-			((HttpServletResponse) response).sendError(404);
-			return null;
-		}
-
-		// remove search robot
-		Pattern robotPattern = (Pattern) this.servlet.getServletContext().getAttribute
-				(SpamFilterTooFreq.BOTNAME);
-		if (robotPattern != null && isPermittedRobot(request, robotPattern)) {
-			response.sendError(404);
-			return null;
-		}
-
-		if (!checkSpamHit(threadId, request)) {
-			((HttpServletResponse) response).sendError(404);
-			return null;
-		}
-
 		try {
 			ForumThread thread = getForumMessageQueryService().getThread(new Long(threadId));
-			if (thread == null) {
-				((HttpServletResponse) response).sendError(404);
-				return null;
-			}
-
-
-			if (!thread.getState().latestPostIsNull()) {
-					thread.addViewCount();
-					getThreadViewCounterJob().saveViewCounter(thread);
-			}
+			ThreadForm threadForm = (ThreadForm) actionForm;
+			PropertyUtils.copyProperties(threadForm, thread);
+			return actionMapping.findForward(FormBeanUtil.FORWARD_SUCCESS_NAME);
 		} catch (Exception e) {
 			Debug.logError(" viewThread error:" + e);
 		}
@@ -76,39 +38,9 @@ public class ViewThreadAction extends Action {
 
 	public ForumMessageQueryService getForumMessageQueryService() {
 		if (forumMessageQueryService == null)
-			forumMessageQueryService = (ForumMessageQueryService) WebAppUtil.getService
-					("forumMessageQueryService", this.servlet.getServletContext());
+			forumMessageQueryService = (ForumMessageQueryService) WebAppUtil.getService("forumMessageQueryService",
+					this.servlet.getServletContext());
 		return forumMessageQueryService;
-	}
-
-
-	public ThreadViewCounterJob getThreadViewCounterJob() {
-		if (threadViewCounterJob == null)
-			threadViewCounterJob = (ThreadViewCounterJob) WebAppUtil.getComponentInstance
-					("threadViewCounterJob", this.servlet.getServletContext());
-		return threadViewCounterJob;
-	}
-
-	private boolean isPermittedRobot(HttpServletRequest request, Pattern robotPattern) {
-		// if refer is null, 1. browser 2. google 3. otherspam
-		String userAgent = request.getHeader("User-Agent");
-		if (robotPattern != null) {
-			if (userAgent != null && userAgent.length() > 0 && robotPattern.matcher(userAgent
-					.toLowerCase()).matches()) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-
-	private boolean checkSpamHit(String id, HttpServletRequest request) {
-		if (customizedThrottle == null) {
-			customizedThrottle = (CustomizedThrottle) WebAppUtil.getComponentInstance
-					("customizedThrottle", this.servlet.getServletContext());
-		}
-		HitKeyIF hitKey = new HitKeySame(request.getRemoteAddr(), "threads");
-		return customizedThrottle.processHitFilter(hitKey);
 	}
 
 }
