ec5de5051ec270a434cb6d3cd941c106d7a2debe
banq <banq@163.com>
2022-06-08 15:16:46 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/spi/component/mapreduce/ThreadDigComparator.java
+++ src/main/java/com/jdon/jivejdon/spi/component/mapreduce/ThreadDigComparator.java
@@ -4,6 +4,7 @@ import com.jdon.jivejdon.domain.model.ForumThread;
 import com.jdon.jivejdon.api.query.ForumMessageQueryService;
 
 import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Like or Give the thumbs-up Comparator
@@ -21,8 +22,14 @@ public class ThreadDigComparator implements Comparator<Long> {
 			return 0;
 		ForumThread thread1 = forumMessageQueryService.getThread(threadId1);
 		ForumThread thread2 = forumMessageQueryService.getThread(threadId2);
-		double thread1Count = thread1.getRootMessage().getDigCount()>0?thread1.getRootMessage().getDigCount() * thread1.getViewCount():thread1.getViewCount();
-		double thread2Count = thread2.getRootMessage().getDigCount()>0?thread2.getRootMessage().getDigCount() * thread2.getViewCount():thread2.getViewCount();
+		double thread1Count = thread1.getRootMessage().getDigCount()>0?thread1.getViewCount() *  (thread1.getRootMessage().getDigCount() + 1):thread1.getViewCount();
+		double thread2Count = thread2.getRootMessage().getDigCount()>0?thread2.getViewCount() *  (thread2.getRootMessage().getDigCount() + 1):thread2.getViewCount();
+
+		long diff1 = TimeUnit.DAYS.convert(Math.abs(System.currentTimeMillis() - thread1.getState().getModifiedDate2()), TimeUnit.MILLISECONDS);
+		long diff2 = TimeUnit.DAYS.convert(Math.abs(System.currentTimeMillis() - thread2.getState().getModifiedDate2()), TimeUnit.MILLISECONDS);
+
+		thread1Count = thread1Count / (diff1 + 1);
+		thread2Count = thread2Count/ (diff2 + 1);
 
 		if (thread1Count > thread2Count)
 			return -1; // returning the first object
