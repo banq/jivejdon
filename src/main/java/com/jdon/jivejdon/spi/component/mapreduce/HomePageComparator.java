32bccefaf0e29ecc49dde2ea72ffad44c4cd431c
banq <banq@163.com>
2022-12-05 05:13:55 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/spi/component/mapreduce/HomePageComparator.java
+++ src/main/java/com/jdon/jivejdon/spi/component/mapreduce/HomePageComparator.java
@@ -47,7 +47,7 @@ public class HomePageComparator implements Comparator<ForumThread> {
 		else if (thread.getRootMessage().getDigCount() > 0)
 			p = Math.pow(p, thread.getRootMessage().getDigCount() + 1) * (3 - diff);
 		long diff2 = thread.getViewCount() - thread.getViewCounter().getLastSavedCount();
-		p = (diff + diff2 + thread.getRootMessage().getDigCount()) * p;
+		p = (diff + thread.getRootMessage().getDigCount()) * p * diff2;
 		return p;
 	}
 }
