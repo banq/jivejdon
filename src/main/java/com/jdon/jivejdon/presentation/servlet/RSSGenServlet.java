2825fec5d4aee0920bae6861da100df44a0f753d
banq <banq@163.com>
2023-09-26 10:15:14 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/presentation/servlet/RSSGenServlet.java
+++ src/main/java/com/jdon/jivejdon/presentation/servlet/RSSGenServlet.java
@@ -79,7 +79,7 @@ public class RSSGenServlet extends HttpServlet {
 	private String channel_title = "";
 	private String channel_des = "";
 	private ServletContext servletContext;
-	private final int LENGTH = 15;
+	private final int LENGTH = 30;
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
