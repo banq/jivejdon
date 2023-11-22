6af0c720d144ef3ebc51d16614bb6e8fd0963d9d
banq <banq@163.com>
2023-11-22 09:18:24 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/domain/model/subscription/notifysubscribed/ForumNotifySubscribed.java
+++ src/main/java/com/jdon/jivejdon/domain/model/subscription/notifysubscribed/ForumNotifySubscribed.java
@@ -55,7 +55,7 @@ public class ForumNotifySubscribed implements NotifySubscribed {
 		// http://www.jdon.com/jivejdon/threadId#messageId
 		String newSubscribedUrl = StringUtil.replace(forumNotifyMessage.getNotifyUrlTemp(), "threadId", message.getForumThread().getThreadId().toString());		
 		String body = message.getForumThread().getName() + " " + Arrays.asList(message.getForumThread().getTagTitles()).stream()
-				.collect(Collectors.joining(" @", " @", " "));
+				.collect(Collectors.joining(" #", " #", " "));
 		shortMessage.setMessageBody(newSubscribedUrl + " " + body.substring(0, body.length() > 120 ? 120 : body.length()) );
 		shortMessage.setMessageTitle(message.getForumThread().getName() + "-" + shortMessage.getMessageTitle());
 
