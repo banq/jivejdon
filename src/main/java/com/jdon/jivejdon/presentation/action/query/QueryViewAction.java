bbb3d73e91f1fe36c532ce2bb7d8e04b28dba6ac
banq <banq@163.com>
2023-12-13 15:36:28 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/presentation/action/query/QueryViewAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/query/QueryViewAction.java
@@ -3,9 +3,6 @@ package com.jdon.jivejdon.presentation.action.query;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.jdon.jivejdon.presentation.form.QueryForm;
-import com.jdon.util.UtilValidate;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.struts.action.Action;
@@ -14,6 +11,8 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionRedirect;
 
+import com.jdon.util.UtilValidate;
+
 public class QueryViewAction extends Action {
 	private final static Logger logger = LogManager.getLogger(ThreadQueryAction.class);
 
@@ -21,28 +20,30 @@ public class QueryViewAction extends Action {
 			HttpServletResponse response) throws Exception {
 		logger.debug("enter ThreadQueryViewAction");
 		String query = request.getParameter("query");
-		if (!UtilValidate.isEmpty(query)) {
-			query = query.replaceAll("[^(a-zA-Z0-9\\u4e00-\\u9fa5)]", "");
-			ActionRedirect redirect = new ActionRedirect(mapping.findForward("search"));
-			redirect.addParameter("query", query);
-			return redirect;
-		}
-		QueryForm qForm = (QueryForm) form;
-		if (UtilValidate.isEmpty(qForm.getQueryType())) {
-			logger.debug("enter view");
+		if (UtilValidate.isEmpty(query))
 			return mapping.findForward("view");
-		} else {// queryType is not empty
-			logger.debug("enter result");
-			if (qForm.getQueryType().equals("messageQueryAction")) {
-				return mapping.findForward("messageQueryAction");
-			}
-			if (qForm.getQueryType().equals("userMessageQueryAction")) {
-				return mapping.findForward("userMessageQueryAction");
-			} else {
-				return mapping.findForward("threadQueryAction");
-			}
+		
+		query = query.replaceAll("[^(a-zA-Z0-9\\s\\u4e00-\\u9fa5)]", "");
+		ActionRedirect redirect = new ActionRedirect(mapping.findForward("search"));
+		redirect.addParameter("query", query);
+		return redirect;
 
-		}
+		// QueryForm qForm = (QueryForm) form;
+		// if (UtilValidate.isEmpty(qForm.getQueryType())) {
+		// logger.debug("enter view");
+		// return mapping.findForward("view");
+		// } else {// queryType is not empty
+		// logger.debug("enter result");
+		// if (qForm.getQueryType().equals("messageQueryAction")) {
+		// return mapping.findForward("messageQueryAction");
+		// }
+		// if (qForm.getQueryType().equals("userMessageQueryAction")) {
+		// return mapping.findForward("userMessageQueryAction");
+		// } else {
+		// return mapping.findForward("threadQueryAction");
+		// }
+
+		// }
 
 	}
 
