a737e63b1da86eedfc2fd0769c49eaaa1c172981
banq <banq@163.com>
2023-12-16 13:16:54 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/domain/model/message/infilter/EscapeUTFInFIlter.java
+++ src/main/java/com/jdon/jivejdon/domain/model/message/infilter/EscapeUTFInFIlter.java
@@ -1,5 +1,6 @@
 package com.jdon.jivejdon.domain.model.message.infilter;
 
+import java.util.Stack;
 import java.util.function.Function;
 
 import com.jdon.jivejdon.domain.model.message.MessageVO;
@@ -19,11 +20,38 @@ public class EscapeUTFInFIlter implements Function<MessageVO, MessageVO> {
 	}
 
 	public String esacpeUtf(String input) {
+		if (isNested(input))
+		     return  input.replaceAll("\\[|\\]", "");
 		return input.replaceAll("[^\\u0000-\\uFFFF]", "");
 		// Pattern unicodeOutliers = Pattern.compile(EmojiRegexUtil.getFullEmojiRegex(),
 		// 		Pattern.UNICODE_CASE | Pattern.CANON_EQ
 		// 				| Pattern.CASE_INSENSITIVE);
 		// Matcher unicodeOutlierMatcher = unicodeOutliers.matcher(input);
 		// return unicodeOutlierMatcher.replaceAll(" ");
+	}
+
+	//from chatGPT
+	private boolean isNested(String text) {
+		Stack<Character> stack = new Stack<>();
+
+		for (char c : text.toCharArray()) {
+			if (c == '[') {
+				stack.push(c);
+			} else if (c == ']') {
+				if (stack.isEmpty()) {
+					return false; // Closing bracket without a matching opening bracket
+				}
+				stack.pop();
+			}
+		}
+
+		return !stack.isEmpty(); // If the stack is empty, brackets are properly nested
+	}
+
+	public static void main(String[] args) {
+		EscapeUTFInFIlter escapeUTFInFIlter = new EscapeUTFInFIlter();
+		String in = "dasdfsdf[sfsa[b]df[/b]sad";
+		if (escapeUTFInFIlter.isNested("dasdfsdf[sfsa[b]df[/b]sad"))
+		     System.out.println("===" + in.replaceAll("\\[|\\]", "ss"));
 	}
 }
