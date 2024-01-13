dc0a0eecf17761b45d0f2a61c4a88df54114bd0f
banq <banq@163.com>
2024-01-14 07:03:01 +0800

    fixed regex not in html tag

--- src/main/java/com/jdon/jivejdon/domain/model/message/output/hotkeys/HotKeysFilter.java
+++ src/main/java/com/jdon/jivejdon/domain/model/message/output/hotkeys/HotKeysFilter.java
@@ -40,9 +40,9 @@ import com.jdon.util.Debug;
 public class HotKeysFilter implements Function<MessageVO, MessageVO> {
 	private final static String module = HotKeysFilter.class.getName();
 
-	private String prefix_regEx = "[^!\"#$%&'()*+,-./:;<=>?@\\[\\]`{|}\\\\~^]"; // chinese or
+	private String prefix_regEx = "(?<!<[^>]{0,10})"; // chinese or
 	// whitespace
-	private String suffix_regEx = "[^!\"#$%&'()*+,-./:;<=>?@\\[\\]`{|}\\\\~^]";
+	private String suffix_regEx = "(?![^>]{0,10}>)";
 
 	private final static ConcurrentHashMap<String, Pattern> patterns = new ConcurrentHashMap<>();
 	//
@@ -95,7 +95,7 @@ public class HotKeysFilter implements Function<MessageVO, MessageVO> {
 		
 		HotKeysFilter hotKeysFilter = new HotKeysFilter();
 		// Example usage
-		String largeText = "啊实Google打实banq注 s注 word.jpg   模在软件工程领域的成就方面品设计的方法/程序，涉及集成或组合较小的独立元素以WORD创建成品。大型产品（如汽车）可以分为更小、更简单的组件，并使用模块化设计方法单独开发和生产。最终产品是通过集成（或组装）这些组件中的banq注每一个来创建的。";
+		String largeText = "啊实Google打实banq注s注word.jpg模在 领域的成就方面品设软件工程计的方word法程序，涉及集成或组合较小的独立元素以WORD创建成品。大型产品（如汽车）可以分为更小、更简单的组件，并使用模块化设计方法单独word开发和生产。最终产品是通过集成（或组装）这些组件中的banq注每一个来创建的。";
 		ConcurrentMap<String, String> searchMap = new ConcurrentHashMap<>();
 		searchMap.put("软件工程", "瞎写");
 		searchMap.put("word", "GPT-4");
