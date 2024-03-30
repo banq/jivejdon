035ebe12474fce1179e99121a055c5b0560c7a3d
banq <banq@163.com>
2024-03-30 10:06:13 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/presentation/filter/IPRequestLimitFilter.java
+++ src/main/java/com/jdon/jivejdon/presentation/filter/IPRequestLimitFilter.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  * from ChatGPT
  */
 public class IPRequestLimitFilter implements Filter {
-    private static final int MAX_REQUESTS_PER_IP = 1; // 最大请求次数
+    private static final int MAX_REQUESTS_PER_IP = 5; // 最大请求次数
     private static final long TIME_WINDOW_MILLIS = 1000; // 时间窗口，单位：毫秒
     private final Map<String, AtomicInteger> ipRequestCounts = new ConcurrentHashMap<>();
 
