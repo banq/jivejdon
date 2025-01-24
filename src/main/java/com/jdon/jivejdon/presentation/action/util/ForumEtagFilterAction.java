f28a823c8980a1d961f80b009d8f4a0055e49227
banq <banq@163.com>
2025-01-25 07:10:05 +0800

    fixed

--- src/main/java/com/jdon/jivejdon/presentation/action/util/ForumEtagFilterAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/util/ForumEtagFilterAction.java
@@ -39,7 +39,7 @@ import com.jdon.jivejdon.util.ToolsUtil;
  * 
  */
 public class ForumEtagFilterAction extends Action {
-	private final static int expire = 5 * 60 * 60;
+	private final static int expire = 1 * 60 * 60;
 
 	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
