98092736d658ad0085231f454bbbd9cd05f2c877
banq <banq@163.com>
2025-05-17 08:33:46 +0800

    refactor MessageListAction to simplify service retrieval and enhance asynchronous processing

--- src/main/java/com/jdon/jivejdon/presentation/action/MessageListAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/MessageListAction.java
@@ -15,16 +15,16 @@
  */
 package com.jdon.jivejdon.presentation.action;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.jdon.controller.WebAppUtil;
@@ -33,7 +33,6 @@ import com.jdon.jivejdon.api.query.ForumMessageQueryService;
 import com.jdon.jivejdon.domain.model.ForumMessage;
 import com.jdon.jivejdon.domain.model.ForumThread;
 import com.jdon.jivejdon.spi.component.mapreduce.ThreadContext;
-import com.jdon.jivejdon.spi.component.viewcount.ThreadViewCounterJob;
 import com.jdon.strutsutil.ModelListAction;
 import com.jdon.strutsutil.ModelListForm;
 import com.jdon.util.Debug;
@@ -44,23 +43,65 @@ import com.jdon.util.Debug;
  */
 public class MessageListAction extends ModelListAction {
 	private final static String module = MessageListAction.class.getName();
-	private ConcurrentMap<String, Object> serviceCache = new ConcurrentHashMap<>();
-	private final ExecutorService executorService = Executors.newFixedThreadPool(5);
-	
-	private ForumMessageQueryService getForumMessageQueryService() {
-		return (ForumMessageQueryService) serviceCache.computeIfAbsent("forumMessageQueryService",
-				k -> WebAppUtil.getService("forumMessageQueryService",
-						this.servlet.getServletContext()));
-	}
 
-	private ThreadViewCounterJob getThreadViewCounterJob() {
-		return (ThreadViewCounterJob) serviceCache.computeIfAbsent("threadViewCounterJob" ,k ->WebAppUtil.getComponentInstance("threadViewCounterJob",
-					this.servlet.getServletContext()));
+	private ForumMessageQueryService getForumMessageQueryService() {
+		return (ForumMessageQueryService) WebAppUtil.getService("forumMessageQueryService",
+				this.servlet.getServletContext());
 	}
 
 	private ThreadContext getThreadContext() {
-		return (ThreadContext) serviceCache.computeIfAbsent("threadContext" ,k ->WebAppUtil.getComponentInstance("threadContext",
-		this.servlet.getServletContext()));
+		return (ThreadContext) WebAppUtil.getComponentInstance("threadContext",
+				this.servlet.getServletContext());
+	}
+
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		ModelListForm listForm = this.getModelListForm(actionMapping, actionForm, request);
+		if (listForm == null) {
+			return actionMapping.findForward("failure");
+		}
+		String threadId = request.getParameter("thread");
+		if (threadId == null || threadId.length() > 12 || !StringUtils.isNumeric(threadId)) {
+			Debug.logError(" getPageIterator error : threadId is null" + threadId, module);
+			return actionMapping.findForward("failure");
+		}
+
+		long threadIdL = Long.parseLong(threadId);
+		ForumThread forumThreadExist = getForumMessageQueryService().getThread(threadIdL);
+		if (forumThreadExist == null) {
+			Debug.logError(" getThread error : thread is gone" + threadId, module);
+			return actionMapping.findForward("failure");
+		}
+
+		CompletableFuture<Void> future1 = CompletableFuture.supplyAsync(() -> {
+			return getForumMessageQueryService().getThread(threadIdL);
+		}).thenAccept(forumThread -> {
+			forumThread.getReBlogVO().loadAscResult();
+			request.setAttribute("threadPreNextList", getThreadContext().getThreadListInContext(forumThread));
+			request.setAttribute("threadLinkList", getThreadContext().createsThreadLinks(forumThread));
+
+		});
+
+		try {
+			if (forumThreadExist.getState().getMessageCount() > 0) {
+				super.execute(actionMapping, listForm, request, response);
+			}else{
+
+				 listForm.setAllCount(1);
+				 List<ForumMessage> list = new ArrayList<>(1);
+				 list.add(forumThreadExist.getRootMessage());
+				 listForm.setList(list);
+
+			}
+		} catch (Exception var9) {
+			Debug.logError(" super.execute err: " + threadId, module);
+		}
+
+		CompletableFuture.allOf(future1).join();
+		listForm.setOneModel(forumThreadExist);
+		
+		return actionMapping.findForward("success");
 	}
 
 	/*
@@ -75,37 +116,12 @@ public class MessageListAction extends ModelListAction {
 		}
 
 		Debug.logVerbose("enter getPageIterator", module);
-		String threadId = request.getParameter("thread");
-		if (threadId == null || threadId.length() > 12 || !StringUtils.isNumeric(threadId)) {
-			Debug.logError(" getPageIterator error : threadId is null" + threadId, module);
-			return new PageIterator();
-		}
-	
+        String threadId = request.getParameter("thread");
 		try {
 			PageIterator pageIterator = getForumMessageQueryService().getMessages(Long.parseLong(threadId), start, count);
-			if(pageIterator == null || pageIterator.getAllCount() == 0)  {
-				Debug.logError(" getPageIterator error : thread is null"  + threadId, module);
-				return new PageIterator();
+			if (pageIterator == null || pageIterator.getAllCount() == 0) {
+				Debug.logError(" getPageIterator error : thread is null" + threadId, module);
 			}
-			
-
-			CompletableFuture<Void> future1 = CompletableFuture.supplyAsync(() -> {
-				return getForumMessageQueryService().getThread(Long.parseLong(threadId));
-			}).thenAccept(forumThread -> {
-				forumThread.getReBlogVO().loadAscResult();
-				request.setAttribute("threadPreNextList",  getThreadContext().getThreadListInContext(forumThread));
-				request.setAttribute("threadLinkList",  getThreadContext().createsThreadLinks(forumThread));
-				
-			});
-	
-
-			// CompletableFuture<Void> future2 = CompletableFuture.supplyAsync(() -> {
-			// 		return getForumMessageQueryService().getThread(Long.parseLong(threadId));
-			// }).thenAccept(forumThread -> {
-			// 	request.setAttribute("threadTagList",  getThreadContext().getThreadListInContext(forumThread));
-			// });
-
-			serviceCache.putIfAbsent(threadId, CompletableFuture.allOf(future1));
 
 			return pageIterator;
 		} catch (Exception nfe) {
@@ -136,32 +152,6 @@ public class MessageListAction extends ModelListAction {
 		return false;
 	}
 
-	public void customizeListForm(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request,
-			ModelListForm modelListForm) throws Exception {
-		String threadId = request.getParameter("thread");
-		if ((threadId == null) || (!StringUtils.isNumeric(threadId)) || threadId.length() > 10) {
-			Debug.logError("customizeListForm error : threadId is null", module);
-			return;
-		}
-
-		try {
-			@SuppressWarnings("unchecked")
-			CompletableFuture<Void> allOf = (CompletableFuture<Void>) serviceCache.get(threadId);
-			allOf.join(); 
-			serviceCache.remove(threadId);
-
-			ForumThread forumThread = getForumMessageQueryService().getThread(Long.parseLong(threadId));
-			if (forumThread == null)
-		    	return;
-
-			modelListForm.setOneModel(forumThread);		
-
-		} catch (Exception e) {
-			Debug.logError(" customizeListForm err:" + threadId, module);
-			return;
-		}
-
-	}
 	
 
 }
