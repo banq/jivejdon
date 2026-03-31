5d9f7170ff4529c724b3cf4b8edb3e6d9795582a
banq <banq@163.com>
2026-03-31 11:06:58 +0800

    fixed ThreadDigListAction

--- src/main/java/com/jdon/jivejdon/presentation/action/query/ThreadDigListAction.java
+++ src/main/java/com/jdon/jivejdon/presentation/action/query/ThreadDigListAction.java
@@ -15,32 +15,24 @@
  */
 package com.jdon.jivejdon.presentation.action.query;
 
-import com.jdon.controller.WebAppUtil;
-import com.jdon.controller.model.PageIterator;
-import com.jdon.jivejdon.spi.component.mapreduce.ThreadApprovedNewList;
-import com.jdon.jivejdon.spi.component.mapreduce.ThreadDigComparator;
-import com.jdon.jivejdon.spi.component.viewcount.ThreadViewCounterJob;
-import com.jdon.jivejdon.api.query.ForumMessageQueryService;
-import com.jdon.jivejdon.domain.model.ForumThread;
-import com.jdon.jivejdon.domain.model.query.ResultSort;
-import com.jdon.jivejdon.domain.model.query.specification.ThreadListSpec;
-import com.jdon.jivejdon.domain.model.query.specification.ThreadListSpecForMod;
-import com.jdon.strutsutil.ModelListForm;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.SortedSet;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.stream.Collectors;
-
+import com.jdon.controller.WebAppUtil;
+import com.jdon.jivejdon.api.query.ForumMessageQueryService;
+import com.jdon.jivejdon.domain.model.ForumThread;
+import com.jdon.jivejdon.spi.component.mapreduce.ThreadApprovedNewList;
+import com.jdon.strutsutil.ModelListForm;
 public class ThreadDigListAction extends Action {
 	private ForumMessageQueryService forumMessageQueryService;
 	private ThreadApprovedNewList threadApprovedNewList;
@@ -71,8 +63,7 @@ public class ThreadDigListAction extends Action {
 		}
 		ModelListForm threadListForm = (ModelListForm) form;
 		List<Long> sortedWindows = getThreadApprovedNewList().getApprovedThreads(0, DigsListMAXSize).stream()
-				.sorted(new ThreadDigComparator(getForumMessageQueryService())).collect(Collectors.toList());
-
+			.sorted(new DigCountComparator(getForumMessageQueryService())).collect(Collectors.toList());
 		
 
 		// 根据排序后的 threadId 获取 ForumThread
@@ -83,5 +74,50 @@ public class ThreadDigListAction extends Action {
 		threadListForm.setList(digThreads);
 		threadListForm.setAllCount(digThreads.size());
 		return mapping.findForward("success");
+	}
+
+	/**
+	 * 按照 ForumThread 的 getDigCount() 结果排序的内部类
+	 */
+	private static class DigCountComparator implements Comparator<Long> {
+		private final ForumMessageQueryService forumMessageQueryService;
+
+		public DigCountComparator(ForumMessageQueryService forumMessageQueryService) {
+			this.forumMessageQueryService = forumMessageQueryService;
+		}
+
+		@Override
+		public int compare(Long threadId1, Long threadId2) {
+			if (threadId1.longValue() == threadId2.longValue())
+				return 0;
+			
+			ForumThread thread1 = forumMessageQueryService.getThread(threadId1);
+			ForumThread thread2 = forumMessageQueryService.getThread(threadId2);
+
+			// 检查 thread1 和 thread2 是否为空
+			if (thread1 == null && thread2 == null) {
+				return 0;
+			} else if (thread1 == null) {
+				return 1;
+			} else if (thread2 == null) {
+				return -1;
+			}
+
+			// 按照 getDigCount() 排序（降序）
+			int digCount1 = thread1.getRootMessage().getMessagePropertysVO().getDigCount();
+			int digCount2 = thread2.getRootMessage().getMessagePropertysVO().getDigCount();
+			
+			if (digCount1 > digCount2)
+				return -1; // 降序排列
+			else if (digCount1 < digCount2)
+				return 1;
+			else {
+				// digCount 相同时，按 threadId 排序
+				if (threadId1.longValue() > threadId2.longValue())
+					return -1;
+				else
+					return 1;
+			}
+		}
 	}
 }
